9d7069f1913502b717d5ddbc4ee96f1949a6b320
garretrc <garretrc@miamioh.edu>
2018-07-02 09:05:47 -0400

    fixed stat_voronoi

--- stat_voronoi.R
+++ stat_voronoi.R
@@ -2,7 +2,7 @@ stat_voronoi <- function(mapping = NULL, data = NULL, geom = "polygon",
                          position = "identity", na.rm = FALSE, show.legend = NA, 
                          inherit.aes = TRUE, outline = NULL, ...) {
   layer(
-    stat = StatLm, data = data, mapping = mapping, geom = geom, 
+    stat = StatVoronoi, data = data, mapping = mapping, geom = geom, 
     position = position, show.legend = show.legend, inherit.aes = inherit.aes,
     params = list(na.rm = na.rm, outline = outline, ...)
   )
