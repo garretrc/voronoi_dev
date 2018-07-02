ae4d7a7af6bcceba09eb8e76311e52868c2ff827
garretrc <garretrc@miamioh.edu>
2018-07-02 14:06:11 -0400

    small improvements to fortify

--- fortify_voronoi.R
+++ fortify_voronoi.R
@@ -1,19 +1,18 @@
-fortify_voronoi = function(vor_spdf,x="x",y="y"){
+fortify_voronoi = function(vor_spdf){
   data=vor_spdf@data
   data$id=rownames(vor_spdf@data)
   
-  data$point.x=data$long
-  data$point.y=data$lat
-  data$long=NULL
-  data$lat=NULL
+  data$point.x=data[,1]
+  data$point.y=data[,2]
+  data[,1:2]=NULL
   data$group=NULL
   
   voronoi = suppressMessages(ggplot2::fortify(vor_spdf, data)) 
   
   voronoi = left_join(voronoi,data,by="id")
   
-  voronoi[,x]=voronoi$long
-  voronoi[,y]=voronoi$lat
+  voronoi$x=voronoi$long
+  voronoi$y=voronoi$lat
   voronoi$long=NULL
   voronoi$lat=NULL
   
