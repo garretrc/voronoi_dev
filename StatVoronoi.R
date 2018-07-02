d00aef5bfb03cd92890e50378405611419696fcc
garretrc <garretrc@miamioh.edu>
2018-07-02 08:38:59 -0400

    updated functino call in StatVoronoi

--- StatVoronoi.R
+++ StatVoronoi.R
@@ -2,6 +2,6 @@ StatVoronoi <- ggproto("StatVoronoi", Stat,
                        required_aes = c("x", "y"),
                        
                        compute_group = function(data, scales, outline = NULL) {
-                         voronoi_polygon(data$x, data$y, data$fill, outline,data.frame=T)
+                         voronoi_polygon(data, "x", "y",outline= outline,data.frame=TRUE)
                        }
 )
